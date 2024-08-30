19ff564c65dc7be67fca4cfd368691d0f5fa445a
henry le <tup16137@temple.edu>
2024-08-30 13:31:41 -0400

    Fixed program due to professor not inserting the correct source code

--- print_random.c
+++ print_random.c
@@ -2,29 +2,23 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-char* rand_string_alloc(size_t size)
+char randchar();
+
+int main()
 {
-    char *s = malloc(size + 1);
-    if (s) {
-        rand_string(s, size);
-    }
-    return s;
-}
+	int a;
 
-int main(){
-    srand(time(NULL)); // Seed the random number generator with the current time
+	/* seed the randomizer */
+	srand( (unsigned)time(NULL) );
 
-    size_t size = 10; // Length of random string
-    char *random_string = rand_string_alloc(size); // Generate random string
+	printf("Today's random word: ");
+	for(a=0;a<7;a++)
+		putchar( randchar() );
+	putchar('\n');
 
-    if (random_string) {
-        printf("Random string: %s\n", random_string);
-    }
-    else{
-        printf("Memory allocation failed\n");
-    }
-    free(random_string); // Free the allocated memory
-    
-    return 0;
+	return(0);
 }
--- random.c
+++ random.c
@@ -4,11 +4,3 @@
 char randchar() {
     return 'A' + (rand() % 26);  // 'A' + (0-25) = 'A'-'Z'
 }
-
-// Function to fill a string with random characters
-void rand_string(char *s, size_t size) {
-    for (size_t i = 0; i < size; i++) {
-        s[i] = randchar();  // Fill the string with random characters
-    }
-    s[size] = '\0';  // Null-terminate the string
-}
