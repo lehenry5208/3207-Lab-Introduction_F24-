a75c09ab0f48909791896cd01ea56ab1ed0b2508
henry le <tup16137@temple.edu>
2024-08-29 23:21:33 -0400

    Added main function, added rand_string function, added touches to print_random.c like including libraries and prototypes

--- random.c
+++ random.c
@@ -1,5 +1,14 @@
-#include <stdio.h>
+#include <stdlib.h>  
+#include <time.h>    
 
-char randchar(){
-    return 'A' + (rand() % 26); // 'A' + (0-25) = 'A'-'Z' 
+char randchar() {
+    return 'A' + (rand() % 26);  // 'A' + (0-25) = 'A'-'Z'
+}
+
+// Function to fill a string with random characters
+void rand_string(char *s, size_t size) {
+    for (size_t i = 0; i < size; i++) {
+        s[i] = randchar();  // Fill the string with random characters
+    }
+    s[size] = '\0';  // Null-terminate the string
 }
